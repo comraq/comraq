98147bd8b600e69885ba41eabd5393a87a599579
adam <adam.yin@pillrcompany.com>
2016-06-05 14:25:57 -0400

    finishing get prop

--- src/functional/getProp.js
+++ src/functional/getProp.js
@@ -1,4 +1,4 @@
-import { isString, isNumber, isUndefined } from "./../utils/checks";
+import { isString, isNumber, isUndefined, isNull } from "./../utils/checks";
 
 import curry from "./curry";
 import composable from "./composable";
@@ -14,18 +14,16 @@ import composable from "./composable";
  *   - value of property from target if found
  *   - null otherwise
  */
-export const getProp = (prop, target) => {
+const getProp = (prop, target) => {
   if (!isString(prop) && !isNumber(prop))
     throw new Error(
       `First argument '${prop}' of getProp must be string or number!`
     );
 
-  if (!isUndefined(target))
+  if (!isUndefined(target) && !isNull(target))
     return (isUndefined(target[prop]))? null: target[prop];
 
   return composable(curry(getProp, prop));
 };
 
-// TODO: getProp, withProp
-export const withProp = () => {};
-
+export default getProp;
--- test/functional.spec.js
+++ test/functional.spec.js
@@ -2,12 +2,12 @@ import currySpecs from "./functional/curry.spec";
 import composableSpecs from "./functional/composable.spec";
 import compositionSpecs from "./functional/composition.spec";
 import arraysSpecs from "./functional/arrays.spec";
-import propSpecs from "./functional/prop.spec";
+import getPropSpecs from "./functional/getProp.spec";
 
 describe("functional:", () => {
   describe("curry:", currySpecs);
   describe("composable:", composableSpecs);
   describe("composition:", compositionSpecs);
   describe("arrays:", arraysSpecs);
-  describe("prop:", propSpecs);
+  describe("getProp:", getPropSpecs);
 });
