5a7ba1703508a54a43d4af8e9e468c9e33b937fe
adam <adam.yin@pillrcompany.com>
2016-06-18 22:13:32 -0400

    added missing link

--- src/functional/transducers/index.js
+++ src/functional/transducers/index.js
@@ -1,3 +1,23 @@
+/**
+ * All of the implementation below are inspired by Clojure's Transducers
+ *
+ * @link http://clojure.org/reference/transducers
+ */
+
+export {
+  default as Transformer, 
+  step, complete, init,
+  isTransformer
+} from "./Transformer";
+
+export {
+  default as Reduced, 
+  deref, isReduced,
+  ensureReduced, ensureUnreduced
+} from "./Reduced";
+
+// TODO
 export const transduce = () => {};
 
+// TODO
 export const into = () => {};
