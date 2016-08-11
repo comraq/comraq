6fc0b6112b0da82023e5e508e24b0d92dc99da49
adam <adam.yin@pillrcompany.com>
2016-08-11 08:22:46 -0400

    adding docstrings for arity

--- src/functional/arity.js
+++ src/functional/arity.js
@@ -1,6 +1,28 @@
 import { isFunction } from "./../utils/checks";
 import { currify, placeholder } from "./curry";
 
+
+/**
+ * @public @function nAry
+ * - given a natural number and a function, returns the function with a
+ *   arity that will be fixed to the provided number
+ *
+ * @param {Number} n
+ * - the number to fix the arity to
+ *
+ * @param {Function} func
+ * - the function to fix the arity for
+ *
+ * @return {Function}
+ * - a new function which will only take n number of arguments and call
+ *   func, returning its value
+ *
+ * @throws TypeError
+ * - if n is not a natural number (less than 0)
+ *
+ * @throws TypeError
+ * - if func is not a function
+ */
 export default currify((n, func) => {
   if (n < 0)
     throw new TypeError(
